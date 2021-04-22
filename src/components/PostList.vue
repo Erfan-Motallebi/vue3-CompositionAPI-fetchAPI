56e32d57e220b927fcbb8f64a9e58ab8d6ac27b9
Erfan <erfan.fulldevs@gmail.com>
2021-04-22 14:30:43 +0430

    new features added

---
+++ src/components/PostList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+      <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+// import { ref } from "vue";
+import SinglePost from "./SinglePost";
+
+export default {
+  name: "postlist",
+  props: ["posts"],
+  components: { SinglePost },
+
+  setup() {},
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="single-post">
+    <h2>{{ post.title }}</h2>
+    <p>{{ snippet }} . . . .</p>
+    <p v-for="tag in post.tags" v-bind:key="tag.id">
+      {{ tag }}
+    </p>
+  </div>
+</template>
+
+<script>
+import { computed } from "@vue/runtime-core";
+export default {
+  name: "singlepost",
+  props: ["post"],
+
+  setup(props) {
+    const snippet = computed(() => {
+      return props.post.body.slice(0, 61);
+    });
+    return { snippet };
+  },
+};
+</script>
+
+<style>
+h2 {
+  color: red;
+  line-height: 3;
+}
+p {
+  text-align: center;
+  width: 50%;
+  margin: auto;
+}
+div > p:nth-of-type(1) ~ p {
+  font-family: "Courier New", Courier, monospace;
+  color: blue;
+}
+</style>
